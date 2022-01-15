56e6fbc5c0df90c4daec55f15f38d3ea2eb91ace
Teamon24 <teamon24@gmail.com>
2022-01-15 22:33:19 +0400

    some refactoring was made.

--- java/src/main/java/core/lambda/exception_handling/Demo.java
+++ java/src/main/java/core/lambda/exception_handling/Demo.java
@@ -10,17 +10,17 @@ import static core.lambda.exception_handling.ThrowingFunction.wrap;
 public class Demo {
     public static void main(String[] args) {
 
-        Class<IOException> ioClass = IOException.class;
-        Class<FileNotFoundException> fnfClass = FileNotFoundException.class;
-        Class<ArithmeticException> aeClass = ArithmeticException.class;
-        Class<OutOfMemoryError> oomClass = OutOfMemoryError.class;
+        Class<IOException>                    ioClass    = IOException.class;
+        Class<FileNotFoundException>          fnfClass   = FileNotFoundException.class;
+        Class<ArithmeticException>            aeClass    = ArithmeticException.class;
+        Class<OutOfMemoryError>               oomClass   = OutOfMemoryError.class;
         Class<ArrayIndexOutOfBoundsException> aioobClass = ArrayIndexOutOfBoundsException.class;
 
-        ThrowingFunction<Object, ?, IOException>                    io     =  (ignored) -> checked(ioClass);
-        ThrowingFunction<Object, ?, FileNotFoundException>          fnf    =  (ignored) -> checked(fnfClass);
-        ThrowingFunction<Object, ?, IOException>                    ae     =  (ignored) -> unchecked(aeClass);
-        ThrowingFunction<Object, ?, ArrayIndexOutOfBoundsException> aioob  =  (ignored) -> unchecked(aeClass);
-        ThrowingFunction<Object, ?, OutOfMemoryError>               oom    =  (ignored) -> error(oomClass);
+        ThrowingFunction<Object, ?, IOException>                    io    = (ignored) -> checked(ioClass);
+        ThrowingFunction<Object, ?, FileNotFoundException>          fnf   = (ignored) -> checked(fnfClass);
+        ThrowingFunction<Object, ?, IOException>                    ae    = (ignored) -> unchecked(aeClass);
+        ThrowingFunction<Object, ?, ArrayIndexOutOfBoundsException> aioob = (ignored) -> unchecked(aeClass);
+        ThrowingFunction<Object, ?, OutOfMemoryError>               oom   = (ignored) -> error(oomClass);
 
         Stream.of(1).forEach(it -> {
                 try { wrap(io,    ioClass).apply(it);    } catch (Throwable t) { message(t); }
