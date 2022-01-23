7d7f549d204cf992875373474dc23a8d2a745fda
Teamon24 <teamon24@gmail.com>
2022-01-23 15:23:24 +0400

    aspectJ example was updated

--- java/docker/aspects.sh
+++ java/docker/aspects.sh
@@ -7,8 +7,7 @@ BUILD="$CURRENT_DIR/build/classes/java/main"
 MODULE_PATH="$CURRENT_DIR/src/main/java"
 EX2_PATH="aop/ex2"
 EX2_ABS_PATH="$MODULE_PATH/$EX2_PATH"
-RESOURCES="$CURRENT_DIR/src/main/resources"
-DEPENDENCIES="$RESOURCES/dependecies"
+DEPENDENCIES="$CURRENT_DIR/src/main/resources/dependecies"
 
 
 APACHE_LANG3="$DEPENDENCIES/commons-lang3-3.12.0.jar"
@@ -20,9 +19,9 @@ logAndEval \
 -showWeaveInfo -1.8"
 
 
-ENTRY_POINT_CLASS_PATH="$EX2_PATH/targets/Main"
-ENTRY_POINT_CLASS_NAME="$(echo "$ENTRY_POINT_CLASS_PATH" | sed -r 's/[/]+/./g')"
+ENTRY_POINT_PATH="$EX2_PATH/targets/Main"
+ENTRY_POINT_NAME="$(echo "$ENTRY_POINT_PATH" | sed -r 's/[/]+/./g')"
 ASPECTJ_JAR="$DEPENDENCIES/aspectjrt-1.9.6.jar"
 logAndEval \
 "java \\
--classpath $BUILD:$ASPECTJ_JAR $ENTRY_POINT_CLASS_NAME"
+-classpath $BUILD:$ASPECTJ_JAR $ENTRY_POINT_NAME"
